ac8e562c8f6fc2dab1be58c52aade74acb0aa3dc
Dewey Sasser <dewey@sasser.com>
2017-02-20 23:07:57 -0500

    Auto-discovery registry base URL.
    If you don't want this to happen, you must not include aws-ecr.mk in your makefile

--- makefiles/aws-ecr.mk
+++ makefiles/aws-ecr.mk
@@ -1,15 +1,13 @@
 # Base URL of Amazon docker regstries
-REGISTRY_BASE?=
-PROFILE=default
+PROFILE?=sandbox
 PUSH=$(if $(REGISTRY_BASE),true,false)
 ECR=aws --profile $(PROFILE) --output text ecr
 
-# In the future, we might want to run production off of a "release" branch
-ifeq (${GIT_BRANCH},release)
-PUSH_TAG=stable
-else
-PUSH_TAG=${TAG}
+ifndef REGISTRY_BASE
+include ecr-registry.mk
 endif
+
+DOCKERFILE=$(wildcard Dockerfile)
 
 PUSHED=$(subst .built,.pushed,$(IMAGES))
 
@@ -18,8 +16,8 @@ all:: $(STATE)/ecr-login $(PUSHED)
 endif
 
 $(STATE)/%.pushed: $(STATE)/%.repo $(STATE)/%.built  | $(STATE)/ecr-login
-	docker tag $*:$(TAG) $(REGISTRY_BASE)/$*:$(PUSH_TAG)
-	docker push $(REGISTRY_BASE)/$*:$(PUSH_TAG)
+	docker tag $*:$(TAG) $(REGISTRY_BASE)/$*:$(TAG)
+	docker push $(REGISTRY_BASE)/$*:$(TAG)
 	touch $@
 
 
@@ -27,7 +25,7 @@ $(STATE)/%.repo: | $(STATE)
 	$(ECR) describe-repositories --query 'repositories[*].repositoryName' --repository-names $* 2>/dev/null || $(ECR) create-repository --repository-name $*
 
 
-cleanup: $(foreach p,$(PUSHED),cleanup/$(notdir $p))
+cleanup:: $(foreach p,$(PUSHED),cleanup/$(notdir $p))
 
 cleanup/%.pushed:
 	for i in $$($(ECR) describe-images --repository-name $* --query imageDetails[*].[imageDigest,imageTags[0]] | awk '/None/{print $$1}' | head -n -2); do $(ECR) batch-delete-image --repository-name $* --image-ids imageDigest=$$i; done
@@ -36,6 +34,7 @@ cleanup/%.pushed:
 info::
 	@echo PUSH=$(PUSH)
 	@echo PUSHED=$(PUSHED)
+	@echo REGISTRY_BASE=$(REGISTRY_BASE)
 
 
 ROUNDED=$(shell echo $$(( $$(date +%s) / (3600*6) * 3600*6 )))
@@ -48,3 +47,5 @@ $(STATE)/ecr-login.%:
 	rm -f $(STATE)/ecr-login.*
 	touch $@
 
+ecr-registry.mk:
+	echo REGISTRY_BASE=$(patsubst %/,%,$(dir $(shell $(ECR) describe-repositories --repository-names inspect --query 'repositories[0].repositoryUri'  2>/dev/null || $(ECR) create-repository --repository-name inspect --query "repositories[0].repositoryUri"))) > $@
